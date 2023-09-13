50f226a335c9b32a7bf5e4d2d1a98c986ea791a3
Gor903 <gor.beglaryan.rw@gmail.com>
2023-09-13 20:33:59 +0400

    /start -> Hellogit ls-files

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -4,6 +4,10 @@ bot = Bot(token="6545393934:AAFOhBNw6w6MqSjcDBr9KgHfZdu6qGlJ6W0")
 dp = Dispatcher(bot)
 
 
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await bot.send_message(message.from_user.id, f'Hello!!')
+
 
 if __name__ == "__main__":
     executor.start_polling(dp)
