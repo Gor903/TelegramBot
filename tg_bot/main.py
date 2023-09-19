30987521bca09531e81da23a13d629ef8d25324b
Gor903 <gor.beglaryan.rw@gmail.com>
2023-09-19 20:00:02 +0400

    Added .env

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -1,20 +1,26 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ContentType
 from TextReader import read_text
+from dotenv import load_dotenv
+import os
 
-bot = Bot(token="6545393934:AAFOhBNw6w6MqSjcDBr9KgHfZdu6qGlJ6W0")
+load_dotenv()
+bot = Bot(token=os.getenv("BOT_TOKEN"))
 dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await bot.send_message(message.from_user.id, f'Hello!!')
+    await bot.send_message(message.from_user.id, f'Hello!👋🏻\n'
+                                                 f'Send me a photo.👀')
 
 
 @dp.message_handler(content_types=ContentType.PHOTO)
 async def photo_handler(message: types.Message):
+    bot_answer = await message.answer("Wait a bit!⏳")
     photo = await message.bot.download_file_by_id(file_id=message.photo[-1].file_id)
     text = read_text(photo)
+    await bot_answer.delete()
     await message.answer(text)
 
 
